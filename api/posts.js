8233912d33d716320265570bf401a08d158a5975
tech-ge <geoffreymaitha044@gmail.com>
2026-03-18 12:18:06 -0500

    Update posts.js

--- api/posts.js
+++ api/posts.js
@@ -8,7 +8,7 @@
  */
 
 const jwt = require('jsonwebtoken');
-const { connectDB, Post } = require('./_db');
+const { connectDB, Post } = require('../lib/db');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'techgeo_secret';
 
